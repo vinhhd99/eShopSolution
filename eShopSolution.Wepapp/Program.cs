2219e5a718efe144cd2535ee584250a8c04620a3
vinhhd99 <hoangvinhvy1@gmail.com>
2022-01-10 19:41:52 +0700

    Add project files.

---
+++ eShopSolution.Wepapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eShopSolution.Wepapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
